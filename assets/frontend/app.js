60a483e16783b47f978a8231541c67946077493e
bruno-oliveira <olivbruno8@gmail.com>
2015-11-17 16:21:49 +0000

    Second commit

--- assets/frontend/app.js
+++ assets/frontend/app.js
@@ -4,6 +4,8 @@ app.controller('RestaurantCtrl', function($scope,$http) {
 
     var cityID = -1;
     $scope.GetCities=function(city) {
+        document.getElementById("restaurantes").removeAttribute("hidden");
+        document.getElementById("estado").removeAttribute("hidden");
         city = document.getElementById("cityname").value;
         alert("Testing API...city is " + city);
         $http.post("http://localhost:3000/cityquery", {"city": city}).success(function (resp) {
@@ -11,7 +13,9 @@ app.controller('RestaurantCtrl', function($scope,$http) {
 
             $http.post("http://localhost:3000/restlist", {"data": [city,resp[1]]}).success(function (resp2) {
 
-                alert("Response after "+resp2["results_found"]);
+                alert("Response after "+resp2["restaurants"].length);
+                document.getElementById("estado").setAttribute("hidden","true");
+                $scope.restName=resp2["restaurants"];
 
             })
 
